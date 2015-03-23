597995c300d06fcf6dabc32a173aefeb808b8bcf
Alexey Strokach <alex.strokach@utoronto.ca>
2015-03-23 00:24:51 -0400

    Added a test which sets up a local database and runs elaspic
    Former-commit-id: f3c17230387738c870c8c7509a70720f052c6397

---
+++ tests/test_database.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 21:29:25 2015
+
+@author: alexey
+"""
+import os
+import imp
+import random
+import re
+import sqlalchemy as sa
+
+from elaspic import conf
+from elaspic import helper_functions as hf
+from elaspic import sql_db
+imp.reload(conf)
+
+import test_elaspic
+imp.reload(test_elaspic)
+
+
+
+#%%
+DEBUG = False
+
+config_filenames = {
+    'mysql': 'config_file_mysql_test.ini',
+    'postgresql': 'config_file_postgresql_test.ini',
+}
+
+try:
+    base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
+except:
+    base_path = os.path.join(os.getcwd(), '../')
+code_path = os.path.join(base_path, 'elaspic/')
+
+print('base_path: {}'.format(base_path))
+
+
+
+#%%
+
+###
+def load_configs(db_type, organism_folder, config_filename):
+    """Load a configuration file
+    """
+    conf.read_configuration_file(os.path.join(base_path, 'config/' + config_filename))
+    configs = conf.configs.copy()
+    configs['organism_folder'] = organism_folder
+    return configs
+    
+
+
+###
+def create_clean_schema(configs, clear_schema=True, keep_uniprot_sequence=False, logger=None):
+    """Create an empty schema in the database defined by the loaded configuration file
+    """
+    imp.reload(sql_db)
+    my_db = sql_db.MyDatabase(configs, logger)
+    try:
+        my_db.create_database_tables(
+            clear_schema=clear_schema, keep_uniprot_sequence=keep_uniprot_sequence)
+    except (sa.exc.OperationalError, sa.exc.ProgrammingError) as e:
+        my_db.logger.error(str(e))
+        if re.search('schema .* does not exist', str(e)):
+            missing_schema = str(e)[str(e).find('schema "')+8:str(e).find('" does not exist')]
+        elif 'Unknown database ' in str(e):
+            missing_schema = str(e)[str(e).find("Unknown database '")+18:str(e).find("'\")")]
+        else:
+            raise e
+        my_db.logger.warning('Missing schema: {}'.format(missing_schema))
+        sql_command = 'create schema {};'.format(missing_schema)
+        my_db.logger.warning("Creating missing schema with system command: '{}'".format(sql_command))
+        my_db.engine.execute(sql_command)
+        my_db.create_database_tables(
+            clear_schema=clear_schema, keep_uniprot_sequence=keep_uniprot_sequence)
+    my_db.logger.info('Done!\n')
+    return my_db
+
+
+
+###
+mysql_load_table_template = (
+    r"""mysql --local-infile --host={db_url} --user={db_username} --password={db_password} """
+    r"""{db_schema} -e "{sql_command}" """
+)
+
+psql_load_table_template = (
+    r"""PGPASSWORD={db_password} psql -h {db_url} -p {db_port} -U {db_username} """
+    r"""-d {db_database} -c "{sql_command}" """
+)
+
+# Need to double up on '\\'
+mysql_command_template = (
+    r"""load data local infile '{organism_folder}/{table_name}.tsv' """
+    r"""into table {db_schema}.{table_name} """
+    r"""fields terminated by '\t' escaped by '\\\\' lines terminated by '\n'; """
+)
+
+psql_command_template = (
+    r"""\\copy {db_schema}.{table_name} """
+    r"""from '{organism_folder}/{table_name}.tsv' """
+    r"""with csv delimiter E'\t' null '\N' escape '\\'; """
+)
+
+def _format_configs(configs, table_name):
+    configs['table_name'] = table_name
+    if table_name == 'uniprot_sequence':
+        configs['db_schema'] = configs['db_schema_uniprot']
+    return configs
+    
+    
+def load_table_to_db(db_type, configs, table_name):
+    configs = _format_configs(configs.copy(), table_name)
+    if db_type == 'mysql':
+        configs['sql_command'] = mysql_command_template.format(**configs)
+        system_command = mysql_load_table_template.format(**configs)
+    elif db_type == 'postgresql':
+        configs['sql_command'] = psql_command_template.format(**configs)
+        system_command = psql_load_table_template.format(**configs)
+    else:
+        raise Exception('Unsupported database type: {}'.format(db_type))
+    if DEBUG:
+        print(system_command)
+    result, error_message, return_code = hf.popen(system_command)
+    if return_code != 0:
+        print(result)
+        raise Exception(error_message)
+
+
+
+###
+table_names = [
+    'domain', 'domain_contact', 
+    'uniprot_sequence', 'provean', 
+    'uniprot_domain', 'uniprot_domain_template', 'uniprot_domain_model', 'uniprot_domain_mutation',
+    'uniprot_domain_pair', 'uniprot_domain_pair_template', 
+    'uniprot_domain_pair_model', 'uniprot_domain_pair_mutation',
+]
+
+def load_all_tables_to_db(db_type, configs):
+    for table_name in table_names:
+        print(table_name)
+        load_table_to_db(db_type, configs, table_name)
+    
+
+
+###
+def delete_schema(my_db, configs, clear_schema, keep_uniprot_sequence):
+    if clear_schema:
+        for table_name in reversed(table_names):
+            if table_name == 'uniprot_sequence':
+                continue
+            configs['table_name'] = table_name
+            my_db.engine.execute('drop table {db_schema}.{table_name} cascade')
+        my_db.engine.execute('drop schema {db_schema};'.format(**configs))
+        
+        if not keep_uniprot_sequence:
+            my_db.engine.execute('drop schema {db_schema_uniprot}.uniprot_sequence cascade;'.format(**configs))
+            
+            if configs['db_schema'] != configs['db_schema_uniprot']:
+                my_db.engine.execute('drop schema {db_schema_uniprot} cascade;'.format(**configs))
+    
+
+
+
+#%%
+def test():
+    DB_TYPE = 'mysql'
+    # DB_TYPE = 'postgresql'
+    # DB_TYPE = 'sqlite'
+    
+    clear_schema = True
+    keep_uniprot_sequence = False
+    result_index = random.randint(0, 100) #87
+    organism_folder = os.path.join(base_path, 'database/Homo_sapiens_test')
+    config_filename = config_filenames[DB_TYPE]
+    logger = hf.get_logger()
+    
+    logger.info('*' * 80)
+    logger.info('Reading the configuration file...')
+    configs = load_configs(DB_TYPE, organism_folder, config_filename)
+    logger.info('*' * 80)
+    logger.info('Creating an empty database schema...')
+    my_db = create_clean_schema(configs, clear_schema, keep_uniprot_sequence, logger)
+    logger.info('*' * 80)
+    logger.info('Loading data from text files to the empty database...')
+    load_all_tables_to_db(DB_TYPE, configs)
+    
+    imp.reload(test_elaspic)
+    logger.info('*' * 80)
+    logger.info('Running a sample domain mutation...')
+    test_uniprot_domain = test_elaspic.TestUniprotDomain()
+    test_uniprot_domain.setup_class(
+        base_path + 'config/' + config_filenames[DB_TYPE], 
+        my_db.engine, configs['db_schema'], configs['db_schema_uniprot'], result_index)
+    test_uniprot_domain.test()
+    
+    logger.info('*' * 80)
+    logger.info('Running a sample interface mutation...')
+    test_uniprot_domain_pair = test_elaspic.TestUniprotDomainPair()
+    test_uniprot_domain_pair.setup_class(
+        base_path + 'config/' + config_filenames[DB_TYPE], 
+        my_db.engine, configs['db_schema'], configs['db_schema_uniprot'], result_index)
+    test_uniprot_domain_pair.test()
+    
+    logger.info('*' * 80)
+    logger.info('Removing the schema that we created for testing')
+    delete_schema(my_db, configs, clear_schema, keep_uniprot_sequence)
+
+
+
+#%%
+if __name__ == '__main__':
+    test()
