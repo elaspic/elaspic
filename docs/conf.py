f1348b84c30f1177cc5637bab1be3fbfe7d0302a
Alexey Strokach <alex.strokach@utoronto.ca>
2015-03-24 13:55:07 -0400

    Updated to sphinx 1.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
