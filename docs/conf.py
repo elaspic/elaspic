adbe1061faa12329eb05e7c3cac32fa1f3a327af
Alexey Strokach <alex.strokach@utoronto.ca>
2015-03-24 12:08:48 -0400

    Updated to a newer version of sphinx and removed sphinx-rtd-theme as a dependency
    Former-commit-id: 69187711a824a9d3bd6ee6d70a0d58a201acf758

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -111,9 +111,8 @@ autodoc_mock_imports = [
 # a list of builtin themes.
 html_theme = "default"
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
