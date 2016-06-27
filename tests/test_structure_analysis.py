65a0f02567fbfe0bfdd90d3c2834ac7a4c330b66
ostrokach <ostrokach@gmail.com>
2016-06-27 00:26:35 -0400

    Add structural_analysis tests.
    Former-commit-id: 2ded25da160524ac0d44740c8d93e5310d8e904e

---
+++ tests/test_structure_analysis.py
@@ -0,0 +1,32 @@
+import os
+import os.path as op
+import tempfile
+import logging
+import elaspic
+
+logger = logging.getLogger(__name__)
+
+
+class TestAnalyseStructure:
+
+    @classmethod
+    def setup_class(cls):
+        pdb_file = op.abspath(op.join(
+            op.splitext(__file__)[0], '4CPA.ENTI_1_PDB4CPA.ENTB_2-4CPAIBIB.pdb'))
+        tempdir = op.join(op.abspath(op.dirname(__file__)), 'tmp')
+        os.makedirs(tempdir, exist_ok=True)
+        tempfile.tempdir = tempdir
+        working_dir = tempfile.mkdtemp()
+        cls.analyse_structure = elaspic.structure_analysis.AnalyzeStructure(
+            pdb_file=pdb_file,
+            working_dir=working_dir,
+            vdw_distance=5.0,
+            min_contact_distance=6.0)
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_get_seasa(self):
+        (seasa_by_chain, seasa_by_chain_separately,
+         seasa_by_residue, seasa_by_residue_separately) = self.analyse_structure.get_seasa()
