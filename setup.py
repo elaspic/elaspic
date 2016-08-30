7a448c3a37d4b6cb4b2591c33ea027aee8a75ee9
ostrokach <ostrokach@gmail.com>
2016-08-30 10:48:50 -0400

    "Release 1.0.25"
    Former-commit-id: 611e4ba9a967f8efe6b7003c4cc738087dbfae20

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class TrainPredictors(Command):
 
 setup(
     name='elaspic',
-    version='1.0.24',
+    version='1.0.25',
     description=META.get('about', {}).get('summary', ''),
     url=META.get('about', {}).get('home', ''),
     author='kimlab',
