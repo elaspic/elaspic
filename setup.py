c36a548e921f72048c0d8c97946a04329c24bacd
ostrokach <ostrokach@gmail.com>
2016-08-31 14:16:53 -0400

    Change version 1.0.30 to 0.1.30, to better represent maturity level.
    Former-commit-id: 9eeec8108232052c215859fc8a37609a717f0863

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class TrainPredictors(Command):
 
 setup(
     name='elaspic',
-    version='1.0.30',
+    version='0.1.30',
     description=(
         "Ensemble Learning Approach for Stability Prediction of "
         "Interface and Core mutations (ELASPIC)."),
