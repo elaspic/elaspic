7714daee38313ccda61b30574a53fa9199650324
ostrokach <ostrokach@gmail.com>
2016-09-05 19:57:44 -0400

    "Release 0.1.35"
    Former-commit-id: 47cd2cda4ee2f172add72dccfc458ad61d6c3d33

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class TrainPredictors(Command):
 
 setup(
     name='elaspic',
-    version='0.1.34',
+    version='0.1.35',
     description=(
         "Ensemble Learning Approach for Stability Prediction of "
         "Interface and Core mutations (ELASPIC)."),
