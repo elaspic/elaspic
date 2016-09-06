5f6fda4e1245be1efeed7d154dcdc86e33e832ee
ostrokach <ostrokach@gmail.com>
2016-09-05 20:10:01 -0400

    Add __version__.
    Former-commit-id: 4b3089811a8d30e19e62aa89d3a35cb16d589e3d

--- elaspic/__init__.py
+++ elaspic/__init__.py
@@ -1,9 +1,12 @@
+__version__ = "0.1.37"
+
 import os
 import os.path as op
 import logging
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
+
 
 BASE_DIR = op.abspath(op.dirname(__file__))
 DATA_DIR = op.join(BASE_DIR, 'data')
