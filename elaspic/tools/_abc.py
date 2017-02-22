615475bc76e5115d723be0a4653392dde47ca894
Alexey Strokach <ostrokach@gmail.com>
2017-02-22 01:47:27 -0500

    Working on Modeller.

--- elaspic/tools/_abc.py
+++ elaspic/tools/_abc.py
@@ -1,13 +1,12 @@
-import functools
+import logging
 import os
 import os.path as op
-import logging
 import tempfile
-
 from abc import ABC, abstractmethod
 
 import Bio
-from kmtools import structure_tools, py_tools
+
+from kmtools import py_tools, structure_tools
 
 logger = logging.getLogger(__name__)
 
@@ -23,17 +22,17 @@ class _Tool(ABC):
     def _result_slots(self):
         raise NotImplementedError
 
-    @abstractmethod
-    def __init__(self):
-        self.result = py_tools.Struct(self._result_slots)
-        self.cache = {}
-        self.mutations = {}
+    def __init__(self, *args, **kwargs):
         self.tempdir = op.join(tempfile.gettempdir(), self.__class__.__name__)
         os.makedirs(self.tempdir, exist_ok=True)
+        self.result = py_tools.Struct(self._result_slots, *args, **kwargs)
+        if args or kwargs:
+            assert self.done
 
     def build(self):
         if not self.done:
             self._build()
+            assert self.done
 
     @abstractmethod
     def _build(self):
@@ -46,10 +45,10 @@ class _Tool(ABC):
                     if key.endswith('file')))
 
 
-class SequenceAnalyzer(_Tool):
+class _SequenceTool(_Tool):
 
-    def __init__(self, sequence):
-        super().__init__()
+    def __init__(self, sequence, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         assert len(sequence) > 0, "The sequence must not be empty!"
         self.sequence = sequence
         self._sequence_file = None
@@ -62,20 +61,11 @@ class SequenceAnalyzer(_Tool):
                 Bio.SeqIO.write(self.sequence, ofh, 'fasta')
         return self._sequence_file
 
-    @functools.lru_cache(maxsize=512)
-    def analyze(self, mutation):
-        assert self.done
-        return self._analyze(self, mutation)
 
-    @abstractmethod
-    def _analyze(self, mutation):
-        raise NotImplementedError
+class _StructureTool(_Tool):
 
-
-class StructureAnalyzer(_Tool):
-
-    def __init__(self, structure):
-        super().__init__()
+    def __init__(self, structure, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         assert len(structure) == 1, "The structure must have only one model!"
         self.structure = structure
         self._structure_file = None
@@ -87,90 +77,40 @@ class StructureAnalyzer(_Tool):
             structure_tools.save_structure(self.structure, self._structure_file)
         return self._structure_file
 
-    @functools.lru_cache(maxsize=512)
-    def analyze(self, chain_id, residue_id, aa):
-        assert self.done
-        if isinstance(residue_id, int):
-            residue_id = (' ', residue_id, ' ')
-        return self._analyze(chain_id, residue_id, aa)
+
+class Modeller(_StructureTool):
+
+    def __init__(self, structure, alignment, *args, **kwargs):
+        super().__init__(structure, *args, **kwargs)
+        self.alignment = alignment
 
     @abstractmethod
-    def _analyze(self, chain_id, residue_id, aa):
+    def model(self):
         raise NotImplementedError
 
 
-class Modeller(_Tool):
+class Mutator(_StructureTool):
 
-    def __init__(self, structure):
-        super().__init__()
-        self.structure = structure
-        self._structure_file = None
+    _result_slots = ['wt_structure_file', 'mut_structure_file']
 
-    @functools.lru_cache(maxsize=512)
-    def mutate(self, mutation):
-        """
-        Returns
-        -------
-        Bio.PDB.Structure
-        """
-        return self._mutate(mutation)
+    def __init__(self, structure, mutation, *args, **kwargs):
+        super().__init__(structure, *args, **kwargs)
+        self.mutation = structure
 
     @abstractmethod
-    def _mutate(self, mutation):
-        raise NotImplementedError
-
-    def model(self, alignment):
-        """
-        Returns
-        -------
-        Bio.PDB.Structure
-        """
-        return self._model(alignment)
-
-    @abstractmethod
-    def _model(self, alignment):
+    def mutate(self):
         raise NotImplementedError
 
 
-class Analyzer(ABC):
-    """Calculate structural properties for a PDB containing one or more chains.
+class SequenceAnalyzer(_SequenceTool):
 
-    Runs the program POPS to calculate the interface size of the complexes
-    This is done by calculating the surface of the complex and the seperated parts.
-    The interface is then given by the substracting.
-    """
-
-    def __init__(self, pdb_file, working_dir, vdw_distance=5.0, min_contact_distance=4.0):
-        self.pdb_file = pdb_file
-        #: Folder with all the binaries (i.e. ./analyze_structure)
-        self.working_dir = working_dir
-        self.vdw_distance = vdw_distance
-        self.min_contact_distance = min_contact_distance
-
-        self._prepare_temp_folder(self.working_dir)
-
-        self.sp = structure_tools.StructureParser()
-        self.sp.extract(pdb_file)
-        self.sp.save_structure(output_dir=self.working_dir)
-
-        self.chain_ids = self.sp.chain_ids
+    @abstractmethod
+    def analyze(self, mutation):
+        raise NotImplementedError
 
 
-class InterfaceAnalyzer(ABC):
+class StructureAnalyzer(_StructureTool):
 
-    def __init__(self, structure, chain_1, residue_1, chain_2, residue_2):
-        self.structure = structure
-        self.chain_1 = chain_1
-        self.residue_1 = residue_1
-        self.chain_2 = chain_2
-        self.residue_2 = residue_2
-        self.tempdir = op.join(tempfile.gettempdir(), self.__class__.__name__)
-        os.makedires(self.tempdir, exist_ok=True)
-
-    def analyze(self):
-        """
-        Returns
-        -------
-        dict : Dictionary containing all the calculated features.
-        """
+    @abstractmethod
+    def analyze(self, mutation):
         raise NotImplementedError
--- elaspic/tools/modeller.py
+++ elaspic/tools/modeller.py
@@ -9,256 +9,230 @@ selection.randomize_xyz
 
 """
 import logging
-import sys
-from contextlib import contextmanager
+import os.path as op
 
+import Bio
+import elaspic
+from Bio.Align import MultipleSeqAlignment
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from elaspic.tools._abc import Modeller, Mutator, ToolError
 from modeller import ModellerError, environ, log, physical
 from modeller.automodel import (assess, automodel, autosched, dope_loopmodel,
                                 refine)
 
-import elaspic
-from elaspic.tools._abc import ToolError
+from kmtools import py_tools
 
 logger = logging.getLogger(__name__)
 ToolError.register(ModellerError)
 
 
-class Modeller:
-    """Run MODELLER in order to make a homology model of the given protein.
+class SaliLabModeller(Modeller):
 
-    Parameters
-    ----------
-    alignment : list
-        Contains filenames with modeller input files in a PIR file format
-    seqID : string
-        Name of the sequence (target)
-    templateID : string
-        Name of the template (structure)
-    filePath : string
-        Path to PDB files
-    modeller_runs : int
-       How many rounds of modelling should be done
-    loopRefinement : boolean
-        If True, calculate loop refinemnts
-    """
-
-    # API
-    def __init__(self, alignment, seqID, templateID, filePath, loopRefinement=True):
-
-        if not isinstance(alignment, list):
-            self.alignment = [alignment]
-        else:
-            self.alignment = alignment
-        self.seqID = seqID
-        self.templateID = templateID
-        self.loopRefinement = loopRefinement
-        self.start = 1  # start model
-        self.end = elaspic.CONFIGS['modeller_runs']  # end model
-        self.loopStart = 1  # start loop refinement model
-        self.loopEnd = 4  # end loop refinement model
-
-        # Some environment settings
-        self.filePath = filePath
+    _result_slots = ['homology_structure_file', 'norm_dope_score']
 
     def _build(self):
-        # key: assessment score
-        # values: alignment, pdb filename, whether or not using loop refinement
-        ranking = dict()
+        self.result['homology_structure_file'], self.result['norm_dope_score'] = (
+            _build_modeller(self.alignment, self.tempdir)
+        )
+
+
+class SaliLabMutator(Mutator):
+
+    _result_slots = ['wt_structure_file', 'mut_structure_file', 'norm_dope_score']
+
+    def _build(self):
+        template_id = self.structure.id + ''.join(chain.id for chain in self.structure[0])
+        template_sequence = '/'.join(chain.sequence for chain in self.structure[0])
+
+        target_id = template_id + '-' + self.mutation
+        target_sequence = []
+        for chain in self.structure[0]:
+            if chain.id == self.mutation.split('-')[0]:
+                assert chain.sequence[:int(self.mutation[1:-1])] == self.mutation.split('-')[1][0]
+                target_sequence.append(
+                    chain.sequence[:int(self.mutation[1:-1])] +
+                    self.mutation[-1] +
+                    chain.sequence[int(self.mutation[1:-1]) + 1:])
+            else:
+                target_sequence.append(chain.sequence)
+        target_sequence = '/'.join(target_sequence)
+
+        self.alignment = MultipleSeqAlignment([
+            SeqRecord(id=template_id, seq=Seq(template_sequence)),
+            SeqRecord(id=target_id, seq=Seq(target_sequence)),
+        ])
+
+        self.result['wt_structure_file'] = self.structure_file
+        self.result['mut_structure_file'], self.result['norm_dope_score'] = (
+            _build_modeller(self.alignment, self.tempdir)
+        )
+
+
+def _build_modeller(alignment, tempdir):
+        results = []
         counter = 0
         max_counter = 3
-        while not ranking and counter < max_counter:
-            logger.debug("counter: %s, ranking: '%s'", counter, ranking)
+        while not results or counter < max_counter:
+            logger.debug("counter: %s, ranking: '%s'", counter, results)
             counter += 1
             # NB: You can actually supply many alignments and modeller will give
             # you the alignment with the best model
-            for aln in self.alignment:
-                # There is a chance that modeller fails to automatically select the
-                # regions for the loop modelling. In that case it tries to model
-                # without loop refinement. Worst thing that could happen is that
-                # the model is bad
+
+            # There is a chance that modeller fails to automatically select the
+            # regions for the loop modelling. In that case it tries to model
+            # without loop refinement. Worst thing that could happen is that
+            # the model is bad
+            try:
+                result, loop, failures = _run_modeller(alignment, tempdir, True)
+            except Exception as e:
+                logger.error('Loop refinement failed with an error: {}'.format(e))
                 try:
-                    result, loop, failures = self.__run_modeller(aln, self.loopRefinement)
+                    result, loop, failures = _run_modeller(alignment, tempdir, False)
+                except ModellerError as e:
+                    raise elaspic.exc.ModellerError(e)
                 except Exception as e:
-                    logger.error('Loop refinement failed with an error: {}'.format(e))
-                    try:
-                        result, loop, failures = self.__run_modeller(aln, False)
-                    except ModellerError as e:
-                        raise elaspic.exc.ModellerError(e)
-                    except Exception as e:
-                        raise e
-                if not result:
-                    raise elaspic.exc.ModellerError(failures[-1])
+                    raise e
+            if not result:
+                raise elaspic.exc.ModellerError(failures[-1])
+            results.extend(result)
+        results.sort(key=lambda x: x[1])
+        return results[0][0], results[0][1]
 
-                for i in range(len(result)):
-                    pdbFile, normDOPE = result[i][0], result[i][1]
-                    ranking[normDOPE] = (aln, pdbFile, loop,)
-        return min(ranking), ranking[min(ranking)][1]
 
-    def _mutate(self, mutation):
-        raise NotImplementedError
+def _run_modeller(alignment, tempdir, loop_refinement):
+    """.
 
-    def _model(self, alignment):
-        raise NotImplementedError
+    Parameters
+    ----------
+    alignFile : string
+        File containing the input data
+    result : list
+        The successfully calculated models are stored in this list
+    loopRefinement : boolean
+        If `True`, perform loop refinements
 
-    # ========= Helper methods ==========
+    Returns
+    -------
+    list
+        Successfully calculated models
+    """
+    alignment_file = _write_alignment(alignment, tempdir)
 
-    def __run_modeller(self, alignFile, loopRefinement):
-        """.
+    log.none()  # instructs Modeller to display no log output.
+    env = environ()  # create a new MODELLER environment to build this model in
 
-        Parameters
-        ----------
-        alignFile : string
-            File containing the input data
-        result : list
-            The successfully calculated models are stored in this list
-        loopRefinement : boolean
-            If `True`, perform loop refinements
+    # Directories for input atom files
+    env.io.atom_files_directory = [tempdir, ]
+    env.schedule_scale = physical.values(default=1.0, soft_sphere=0.7)
 
-        Returns
-        -------
-        list
-            Successfully calculated models
-        """
-        log.none()  # instructs Modeller to display no log output.
-        env = environ()  # create a new MODELLER environment to build this model in
+    # Selected atoms do not feel the neighborhood
+    # env.edat.nonbonded_sel_atoms = 2
+    env.io.hetatm = True  # read in HETATM records from template PDBs
+    env.io.water = True  # read in WATER records (including waters marked as HETATMs)
 
-        # Directories for input atom files
-        env.io.atom_files_directory = [str(self.filePath.rstrip('/')), ]
-        env.schedule_scale = physical.values(default=1.0, soft_sphere=0.7)
-
-        # Selected atoms do not feel the neighborhood
-        # env.edat.nonbonded_sel_atoms = 2
-        env.io.hetatm = True  # read in HETATM records from template PDBs
-        env.io.water = True  # read in WATER records (including waters marked as HETATMs)
-
-        logger.debug(
-            'Performing loop refinement in addition to regular modelling: {}'
-            .format(loopRefinement)
+    logger.debug(
+        'Performing loop refinement in addition to regular modelling: {}'
+        .format(loop_refinement)
+    )
+    if not loop_refinement:
+        a = automodel(
+            env,
+            # alignment filename
+            alnfile=alignment_file,
+            # codes of the templates
+            knowns=(alignment[0].id, ),
+            # code of the target
+            sequence=alignment[1].id,
+            # wich method for validation should be calculated
+            assess_methods=(assess.DOPE, assess.normalized_dope)
         )
-        if not loopRefinement:
-            a = automodel(
-                env,
-                # alignment filename
-                alnfile=str(alignFile),
-                # codes of the templates
-                knowns=(str(self.templateID)),
-                # code of the target
-                sequence=str(self.seqID),
-                # wich method for validation should be calculated
-                assess_methods=(assess.DOPE, assess.normalized_dope)
-            )
+    else:
+        a = dope_loopmodel(
+            env,
+            # alignment filename
+            alnfile=alignment_file,
+            # codes of the templates
+            knowns=(alignment[0].id, ),
+            # code of the target
+            sequence=alignment[1].id,
+            # wich method for validation should be calculated
+            assess_methods=(assess.DOPE, assess.normalized_dope),
+            loop_assess_methods=(assess.DOPE, assess.normalized_dope)
+        )
+        # index of the first loop model
+        a.loop.starting_model = 1  # self.loopStart
+        # index of the last loop model
+        a.loop.ending_model = 4  # self.loopEnd
+        # loop refinement method; this yields
+        a.loop.md_level = refine.slow
+
+    a.starting_model = 1  # self.start  # index of the first model
+    a.ending_model = elaspic.CONFIGS.get('modeller_runs', 1)  # index of the last model
+
+    # Very thorough VTFM optimization:
+    a.library_schedule = autosched.slow
+    a.max_var_iterations = 300
+
+    # Thorough MD optimization:
+    # a.md_level = refine.slow
+    a.md_level = None
+
+    # Repeat the whole cycle 2 times and do not stop unless obj.func. > 1E6
+    # a.repeat_optimization = 2
+
+    a.max_molpdf = 2e5
+
+    with py_tools.log_print_statements(logger):
+        a.make()  # do the actual homology modeling
+
+    # The output produced by modeller is stored in a.loop.outputs or a.outputs
+    # it is a dictionary
+    # Check for each model if it was successfully calculated, i.e.
+    # for each "normal" model and each loop model and append the
+    # assessment score to a list which is used to return the best model
+    result = []
+    loop = False
+    failures = []
+    # Add the normal output
+    for i in range(len(a.outputs)):
+        if not a.outputs[i]['failure']:
+            model_filename = a.outputs[i]['name']
+            model_dope_score = a.outputs[i]['Normalized DOPE score']
+            logger.debug(
+                'Success! model_filename: {}, model_dope_score: {}'
+                .format(model_filename, model_dope_score))
+            result.append((model_filename, model_dope_score))
         else:
-            a = dope_loopmodel(
-                env,
-                # alignment filename
-                alnfile=str(alignFile),
-                # codes of the templates
-                knowns=(str(self.templateID)),
-                # code of the target
-                sequence=str(self.seqID),
-                # wich method for validation should be calculated
-                assess_methods=(assess.DOPE, assess.normalized_dope),
-                loop_assess_methods=(assess.DOPE, assess.normalized_dope)
-            )
-            # index of the first loop model
-            a.loop.starting_model = self.loopStart
-            # index of the last loop model
-            a.loop.ending_model = self.loopEnd
-            # loop refinement method; this yields
-            a.loop.md_level = refine.slow
+            failure = a.outputs[i]['failure']
+            logger.debug('Failure! {}'.format(failure))
+            failures.append(a.outputs[i]['failure'])
 
-        a.starting_model = self.start  # index of the first model
-        a.ending_model = self.end  # index of the last model
-
-        # Very thorough VTFM optimization:
-        a.library_schedule = autosched.slow
-        a.max_var_iterations = 300
-
-        # Thorough MD optimization:
-        # a.md_level = refine.slow
-        a.md_level = None
-
-        # Repeat the whole cycle 2 times and do not stop unless obj.func. > 1E6
-        # a.repeat_optimization = 2
-
-        a.max_molpdf = 2e5
-
-        with log_print_statements(logger):
-            a.make()  # do the actual homology modeling
-
-        # The output produced by modeller is stored in a.loop.outputs or a.outputs
-        # it is a dictionary
-        # Check for each model if it was successfully calculated, i.e.
-        # for each "normal" model and each loop model and append the
-        # assessment score to a list which is used to return the best model
-        result = []
-        loop = False
-        failures = []
-        # Add the normal output
-        for i in range(len(a.outputs)):
-            if not a.outputs[i]['failure']:
-                model_filename = a.outputs[i]['name']
-                model_dope_score = a.outputs[i]['Normalized DOPE score']
+    # Add the loop refinement output
+    if loop_refinement:
+        logger.debug('Modeller loop outputs:')
+        for i in range(len(a.loop.outputs)):
+            if not a.loop.outputs[i]['failure']:
+                model_filename = a.loop.outputs[i]['name']
+                model_dope_score = a.loop.outputs[i]['Normalized DOPE score']
                 logger.debug(
                     'Success! model_filename: {}, model_dope_score: {}'
                     .format(model_filename, model_dope_score))
                 result.append((model_filename, model_dope_score))
+                loop = True
             else:
-                failure = a.outputs[i]['failure']
+                failure = a.loop.outputs[i]['failure']
                 logger.debug('Failure! {}'.format(failure))
-                failures.append(a.outputs[i]['failure'])
+                failures.append(failure)
 
-        # Add the loop refinement output
-        if loopRefinement:
-            logger.debug('Modeller loop outputs:')
-            for i in range(len(a.loop.outputs)):
-                if not a.loop.outputs[i]['failure']:
-                    model_filename = a.loop.outputs[i]['name']
-                    model_dope_score = a.loop.outputs[i]['Normalized DOPE score']
-                    logger.debug(
-                        'Success! model_filename: {}, model_dope_score: {}'
-                        .format(model_filename, model_dope_score))
-                    result.append((model_filename, model_dope_score))
-                    loop = True
-                else:
-                    failure = a.loop.outputs[i]['failure']
-                    logger.debug('Failure! {}'.format(failure))
-                    failures.append(failure)
-
-        # Return the successfully calculated models and a loop flag indicating
-        # whether the returned models are loop refined or not
-        return result, loop, failures
+    # Return the successfully calculated models and a loop flag indicating
+    # whether the returned models are loop refined or not
+    return result, loop, failures
 
 
-# Logging
-class WritableObject:
-    """A writable object which writes everything to the logger."""
-
-    def __init__(self, logger):
-        self.logger = logger
-
-    def write(self, string):
-        self.logger.debug(string.strip())
-
-
-@contextmanager
-def log_print_statements(logger):
-    """Channel print statements to the debug logger.
-
-    Useful for modules that default to printing things instead of using a logger (Modeller...).
-    """
-    original_stdout = sys.stdout
-    original_formatters = []
-    for i in range(len(logger.handlers)):
-        original_formatters.append(logger.handlers[0].formatter)
-        logger.handlers[i].formatter = logging.Formatter('%(message)s')
-    wo = WritableObject(logger)
-    try:
-        sys.stdout = wo
-        yield
-    except:
-        raise
-    finally:
-        sys.stdout = original_stdout
-        for i in range(len(logger.handlers)):
-            logger.handlers[i].formatter = original_formatters[i]
+def _write_alignment(alignment, tempdir):
+    assert len(alignment) == 2
+    alignment_file = op.join(tempdir, alignment[0].id + '-' + alignment[1].id + '.pir')
+    with open(alignment_file, 'wt') as ofh:
+        Bio.SeqIO.write(alignment, ofh, 'pir')
+    return alignment_file
