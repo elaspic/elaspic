9f28863bb0d68a569c619e4e6c5ef4bed11b5753
ostrokach <ostrokach@gmail.com>
2017-05-17 18:54:20 -0400

    Release 0.1.41

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class TrainPredictors(Command):
 
 setup(
     name='elaspic',
-    version='0.1.40',
+    version='0.1.41',
     description=(
         "Ensemble Learning Approach for Stability Prediction of "
         "Interface and Core mutations (ELASPIC)."),
