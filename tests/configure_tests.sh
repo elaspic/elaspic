4976c46eddb9f7821c1585ec27c7d3cc147d66d1
ostrokach <ostrokach@gmail.com>
2017-05-18 14:41:46 -0400

    Don't configure database from configure_tests.hs

---
+++ tests/configure_tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ev
+
+# Sanity checks
+if [[ -z ${SRC_DIR} || \
+      -z ${SCRIPTS_DIR} || \
+      -z ${TEST_DIR} ]] ; then
+    echo 'Required environment variable not set!'
+    exit
+fi
+
+if [[ ${SRC_DIR} = ${TEST_DIR} ]] ; then
+    echo "SRC_DIR and TEST_DIR must be different!"
+    exit
+fi
+
+
+# Copy package data
+mkdir -p "${TEST_DIR}"
+rsync -av "${SRC_DIR}/tests/" "${TEST_DIR}" --exclude='[._]*'
+ls ${TEST_DIR}
+
+
+# Common directories
+export PDB_DIR="${TEST_DIR}/pdb"
+mkdir -p "${PDB_DIR}"
+
+export BLAST_DB_DIR="${TEST_DIR}/blast/db"
+mkdir -p "${BLAST_DB_DIR}"
+touch "${BLAST_DB_DIR}/nr.pal"
+touch "${BLAST_DB_DIR}/pdbaa.pal"
