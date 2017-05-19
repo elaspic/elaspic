ed23cc0b62e7ef6bbcdf25a8e402ad93f4593afa
ostrokach <ostrokach@gmail.com>
2017-05-18 20:03:04 -0400

    Add script for making it easier to run gitlab-ci scripts locally

---
+++ run_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -z $ANACONDA_TOKEN || \
+      -z $KEY_MODELLER || \
+      -z $1 ]] ; then
+  echo "Required environment variables have not been set!"
+  exit -1
+fi
+
+gitlab-runner exec docker \
+    --env ANACONDA_TOKEN="${ANACONDA_TOKEN}" \
+    --env KEY_MODELLER="${KEY_MODELLER}" \
+    --env CI_PROJECT_NAME=elaspic \
+    --docker-volumes `pwd`/conda-bld/linux-64:/opt/conda/conda-bld/linux-64 \
+    $1
+
