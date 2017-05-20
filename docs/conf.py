4c61d2afdcb60436c82c1b896a59e8817a52792a
ostrokach <ostrokach@gmail.com>
2017-05-20 15:31:36 -0400

    Don't modify sys.path when building docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 # serve to show the default.
 
 import os
-import sys
+# import sys
 # from sphinx.ext import autodoc
 
 
@@ -40,7 +40,7 @@ def setup(app):
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
