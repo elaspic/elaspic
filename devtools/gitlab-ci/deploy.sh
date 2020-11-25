3f1080f33eb9104f26bed7ed7a55861a3006bb7b
Alexey Strokach <alex.strokach@utoronto.ca>
2020-11-25 14:42:27 -0500

    Change anaconda channel

--- devtools/gitlab-ci/deploy.sh
+++ devtools/gitlab-ci/deploy.sh
@@ -4,13 +4,13 @@ case "${PACKAGE_VERSION}" in
   *dev*)
     anaconda
       -t $ANACONDA_TOKEN upload $CI_PROJECT_DIR/conda-bld/linux-64/*.tar.bz2
-      -u ${CI_PROJECT_NAMESPACE}
+      -u kimlab
       --label dev
       --force
   ;;
   *)
     anaconda
       -t $ANACONDA_TOKEN upload $CI_PROJECT_DIR/conda-bld/linux-64/*.tar.bz2
-      -u ${CI_PROJECT_NAMESPACE}
+      -u kimlab
   ;;
 esac
