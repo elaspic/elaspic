26b58d91f9eff5ad98458299c7e6054567248c8a
Alexey Strokach <alex.strokach@utoronto.ca>
2020-11-25 16:43:11 -0500

    Update deploy.sh to upload noarch packages

--- devtools/gitlab-ci/deploy.sh
+++ devtools/gitlab-ci/deploy.sh
@@ -3,14 +3,14 @@
 case "${PACKAGE_VERSION}" in
   *dev*)
     anaconda \
-      -t $ANACONDA_TOKEN upload $CI_PROJECT_DIR/conda-bld/linux-64/*.tar.bz2 \
+      -t $ANACONDA_TOKEN upload ${CI_PROJECT_DIR}/conda-bld/noarch/*.tar.bz2 \
       -u kimlab \
       --label dev \
       --force
   ;;
   *)
     anaconda \
-      -t $ANACONDA_TOKEN upload $CI_PROJECT_DIR/conda-bld/linux-64/*.tar.bz2 \
+      -t $ANACONDA_TOKEN upload ${CI_PROJECT_DIR}/conda-bld/noarch/*.tar.bz2 \
       -u kimlab
   ;;
 esac
