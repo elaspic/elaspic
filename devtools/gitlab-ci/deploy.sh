caa73091d054b1798d91fc34a5cc421e59dd1e59
Alexey Strokach <alex.strokach@utoronto.ca>
2020-11-25 15:30:29 -0500

    Fix deploy script

--- devtools/gitlab-ci/deploy.sh
+++ devtools/gitlab-ci/deploy.sh
@@ -2,15 +2,15 @@
 
 case "${PACKAGE_VERSION}" in
   *dev*)
-    anaconda
-      -t $ANACONDA_TOKEN upload $CI_PROJECT_DIR/conda-bld/linux-64/*.tar.bz2
-      -u kimlab
-      --label dev
+    anaconda \
+      -t $ANACONDA_TOKEN upload $CI_PROJECT_DIR/conda-bld/linux-64/*.tar.bz2 \
+      -u kimlab \
+      --label dev \
       --force
   ;;
   *)
     anaconda
-      -t $ANACONDA_TOKEN upload $CI_PROJECT_DIR/conda-bld/linux-64/*.tar.bz2
+      -t $ANACONDA_TOKEN upload $CI_PROJECT_DIR/conda-bld/linux-64/*.tar.bz2 \
       -u kimlab
   ;;
 esac
