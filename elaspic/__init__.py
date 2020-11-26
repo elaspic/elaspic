61db6a86bcb5ed21a7928d6bb6b568864a147798
Alexey Strokach <alex.strokach@utoronto.ca>
2020-11-25 22:56:04 -0500

    "Release 0.1.48.dev0"

--- elaspic/__init__.py
+++ elaspic/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.47"
+__version__ = "0.1.48.dev0"
 
 import logging
 import os
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class TrainPredictors(Command):
 
 setup(
     name="elaspic",
-    version="0.1.47",
+    version="0.1.48.dev0",
     description=(
         "Ensemble Learning Approach for Stability Prediction of "
         "Interface and Core mutations (ELASPIC)."
